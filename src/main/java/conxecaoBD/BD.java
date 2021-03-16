e189cea5c8f4857992b223a04ce1467ee39b4fb9
lucas silva <55859221+LucasJesus17@users.noreply.github.com>
2021-03-16 11:18:20 -0300

    conexao com o banco

---
+++ src/main/java/conxecaoBD/BD.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conxecaoBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author kinha
+ */
+public class BD {
+
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                Properties prop = new Properties();
+                
+                String user = "root";
+                String password = "";
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Pi-4", user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    }
+} 
+
