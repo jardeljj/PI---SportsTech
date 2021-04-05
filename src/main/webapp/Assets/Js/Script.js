29ce5aa6d18b6adee3abed8f542fb3ab59ac1838
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2021-04-05 19:38:25 -0300

    crud cliente

--- src/main/webapp/Assets/Js/Script.js
+++ src/main/webapp/Assets/Js/Script.js
@@ -1,198 +1,335 @@
 class Validator {
 
     constructor() {
-      this.validations = [
-        'data-min-length',
-        'data-max-length',
-        'data-only-letters',
-        'data-email-validate',
-        'data-required',
-        'data-equal',
-        'data-password-validate',
-      ]
+        this.validations = [
+            'data-min-length',
+            'data-max-length',
+            'data-only-letters',
+            'data-email-validate',
+            'data-required',
+            'data-equal',
+            'data-password-validate',
+        ]
     }
-  
+
     // inicia a validação de todos os campos
     validate(form) {
-  
-      // limpa todas as validações antigas
-      let currentValidations = document.querySelectorAll('form .error-validation');
-  
-      if(currentValidations.length) {
-        this.cleanValidations(currentValidations);
-      }
-  
-      // pegar todos inputs
-      let inputs = form.getElementsByTagName('input');
-      // transformar HTMLCollection em arr
-      let inputsArray = [...inputs];
-      // loop nos inputs e validação mediante aos atributos encontrados
-      inputsArray.forEach(function(input, obj) {
-  
-        // fazer validação de acordo com o atributo do input
-        for(let i = 0; this.validations.length > i; i++) {
-          if(input.getAttribute(this.validations[i]) != null) {
-  
-            // limpa string para saber o método
-            let method = this.validations[i].replace("data-", "").replace("-", "");
-  
-            // valor do input
-            let value = input.getAttribute(this.validations[i])
-  
-            // invoca o método
-            this[method](input,value);
-  
-          }
+
+        // limpa todas as validações antigas
+        let currentValidations = document.querySelectorAll('form .error-validation');
+
+        if (currentValidations.length) {
+            this.cleanValidations(currentValidations);
         }
-  
-      }, this);
-  
+
+        // pegar todos inputs
+        let inputs = form.getElementsByTagName('input');
+        // transformar HTMLCollection em arr
+        let inputsArray = [...inputs];
+        // loop nos inputs e validação mediante aos atributos encontrados
+        inputsArray.forEach(function (input, obj) {
+
+            // fazer validação de acordo com o atributo do input
+            for (let i = 0; this.validations.length > i; i++) {
+                if (input.getAttribute(this.validations[i]) != null) {
+
+                    // limpa string para saber o método
+                    let method = this.validations[i].replace("data-", "").replace("-", "");
+
+                    // valor do input
+                    let value = input.getAttribute(this.validations[i])
+
+                    // invoca o método
+                    this[method](input, value);
+
+                }
+            }
+
+        }, this);
+
     }
-  
+
     // método para validar se tem um mínimo de caracteres
     minlength(input, minValue) {
-      
 
-      let inputLength = input.value.length;
-  
-      let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
-  
-      if(inputLength < minValue) {
-        this.printMessage(input, errorMessage);
-      }
-  
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
+        if (inputLength < minValue) {
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // método para validar se passou do máximo de caracteres
     maxlength(input, maxValue) {
-  
-      let inputLength = input.value.length;
-  
-      let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
-  
-      if(inputLength > maxValue) {
-        this.printMessage(input, errorMessage);
-      }
-  
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
+
+        if (inputLength > maxValue) {
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // método para validar strings que só contem letras
     onlyletters(input) {
-  
-      let re = /^[A-Za-z]+$/;;
-  
-      let inputValue = input.value;
-  
-      let errorMessage = `Este campo não aceita números nem caracteres especiais`;
-  
-      if(!re.test(inputValue)) {
-        this.printMessage(input, errorMessage);
-      }
-  
+
+        let re = /^[A-Za-z]+$/;
+        ;
+
+        let inputValue = input.value;
+
+        let errorMessage = `Este campo não aceita números nem caracteres especiais`;
+
+        if (!re.test(inputValue)) {
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // método para validar e-mail
     emailvalidate(input) {
-      let re = /\S+@\S+\.\S+/;
-  
-      let email = input.value;
-  
-      let errorMessage = `Insira um e-mail no padrão matheus@email.com`;
-  
-      if(!re.test(email)) {
-        this.printMessage(input, errorMessage);
-      }
-  
+        let re = /\S+@\S+\.\S+/;
+
+        let email = input.value;
+
+        let errorMessage = `Insira um e-mail no padrão matheus@email.com`;
+
+        if (!re.test(email)) {
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // verificar se um campo está igual o outro
     equal(input, inputName) {
-  
-      let inputToCompare = document.getElementsByName(inputName)[0];
-  
-      let errorMessage = `Este campo precisa estar igual ao ${inputName}`;
-  
-      if(input.value != inputToCompare.value) {
-        this.printMessage(input, errorMessage);
-      }
+
+        let inputToCompare = document.getElementsByName(inputName)[0];
+
+        let errorMessage = `Este campo precisa estar igual ao ${inputName}`;
+
+        if (input.value != inputToCompare.value) {
+            this.printMessage(input, errorMessage);
+        }
     }
-    
+
     // método para exibir inputs que são necessários
     required(input) {
-  
-      let inputValue = input.value;
-  
-      if(inputValue === '') {
-        let errorMessage = `Este campo é obrigatório`;
-  
-        this.printMessage(input, errorMessage);
-      }
-  
+
+        let inputValue = input.value;
+
+        if (inputValue === '') {
+            let errorMessage = `Este campo é obrigatório`;
+
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // validando o campo de senha
     passwordvalidate(input) {
-  
-      // explodir string em array
-      let charArr = input.value.split("");
-  
-      let uppercases = 0;
-      let numbers = 0;
-  
-      for(let i = 0; charArr.length > i; i++) {
-        if(charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
-          uppercases++;
-        } else if(!isNaN(parseInt(charArr[i]))) {
-          numbers++;
+
+        // explodir string em array
+        let charArr = input.value.split("");
+
+        let uppercases = 0;
+        let numbers = 0;
+
+        for (let i = 0; charArr.length > i; i++) {
+            if (charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
+                uppercases++;
+            } else if (!isNaN(parseInt(charArr[i]))) {
+                numbers++;
+            }
         }
-      }
-  
-      if(uppercases === 0 || numbers === 0) {
-        let errorMessage = `A senha precisa um caractere maiúsculo e um número`;
-  
-        this.printMessage(input, errorMessage);
-      }
-  
+
+        if (uppercases === 0 || numbers === 0) {
+            let errorMessage = `A senha precisa um caractere maiúsculo e um número`;
+
+            this.printMessage(input, errorMessage);
+        }
+
     }
-  
+
     // método para imprimir mensagens de erro
     printMessage(input, msg) {
-    
-      // checa os erros presentes no input
-      let errorsQty = input.parentNode.querySelector('.error-validation');
-  
-      // imprimir erro só se não tiver erros
-      if(errorsQty === null) {
-        let template = document.querySelector('.error-validation').cloneNode(true);
-  
-        template.textContent = msg;
-    
-        let inputParent = input.parentNode;
-    
-        template.classList.remove('template');
-    
-        inputParent.appendChild(template);
-      }
-  
+
+        // checa os erros presentes no input
+        let errorsQty = input.parentNode.querySelector('.error-validation');
+
+        // imprimir erro só se não tiver erros
+        if (errorsQty === null) {
+            let template = document.querySelector('.error-validation').cloneNode(true);
+
+            template.textContent = msg;
+
+            let inputParent = input.parentNode;
+
+            template.classList.remove('template');
+
+            inputParent.appendChild(template);
+        }
+
     }
-  
+
     // remove todas as validações para fazer a checagem novamente
     cleanValidations(validations) {
-      validations.forEach(el => el.remove());
+        validations.forEach(el => el.remove());
     }
-  
-  }
-  
-  let form = document.getElementById('register-form');
-  let submit = document.getElementById('btn-submit');
-  
-  let validator = new Validator();
-  
-  // evento de envio do form, que valida os inputs
-  submit.addEventListener('click', function(e) {
+
+}
+
+let form = document.getElementById('register-form');
+let submit = document.getElementById('btn-submit');
+
+let validator = new Validator();
+
+// evento de envio do form, que valida os inputs
+submit.addEventListener('click', function (e) {
     e.preventDefault();
 
     validator.validate(form);
-  });
-  
+});
+
+function limpa_formulario_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('estado').value = ("");
+
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('estado').value = (conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulario_cep();
+        alert("CEP não encontrado.");
+        document.getElementById('cep').value = ("");
+    }
+}
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('estado').value = "...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulario_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulario_cep();
+    }
+}
+
+function formatar(mascara, documento) {
+    var i = documento.value.length;
+    var saida = mascara.substring(0, 1);
+    var texto = mascara.substring(i);
+
+    if (texto.substring(0, 1) != saida) {
+        documento.value += texto.substring(0, 1);
+    }
+
+}
+
+function idade() {
+    var data = document.getElementById("dtnasc").value;
+    var dia = data.substr(0, 2);
+    var mes = data.substr(3, 2);
+    var ano = data.substr(6, 4);
+    var d = new Date();
+    var ano_atual = d.getFullYear(),
+            mes_atual = d.getMonth() + 1,
+            dia_atual = d.getDate();
+
+    ano = +ano,
+            mes = +mes,
+            dia = +dia;
+
+    var idade = ano_atual - ano;
+
+    if (mes_atual < mes || mes_atual == mes_aniversario && dia_atual < dia) {
+        idade--;
+    }
+    return idade;
+}
+
+
+function exibe(i) {
+
+
+
+    document.getElementById(i).readOnly = true;
+
+
+
+
+}
+
+function desabilita(i) {
+
+    document.getElementById(i).disabled = true;
+}
+function habilita(i)
+{
+    document.getElementById(i).disabled = false;
+}
+
+
+function showhide()
+{
+    var div = document.getElementById("newpost");
+
+    if (idade() >= 18) {
+
+        div.style.display = "none";
+    } else if (idade() < 18) {
+        div.style.display = "inline";
+    }
+
+}
+
+
   
---
+++ src/main/webapp/html/CadastroCliente.jsp
@@ -0,0 +1,182 @@
+<%-- 
+    Document   : CadastroCliente
+    Created on : 3 de abr de 2021, 13:08:19
+    Author     : Jardel
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+              integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+              integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../Assets/css/Style.css">
+        <title>Cadastro de Cliente</title>
+    </head
+    <%@include file="../Header.jsp" %>
+    <body>
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="panel panel-primary">
+                    <div class="panel-body">
+                        <div class="form-group">
+
+                            <div class="col-md-11 control-label">
+                                <p class="help-block"><h11>*</h11> Campo Obrigatório </p>
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>  
+                            <div class="col-md-8">
+                                <input id="Nome" name="Nome" placeholder="" class="form-control input-md" required="" type="text">
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="Nome">CPF <h11>*</h11></label>  
+                            <div class="col-md-2">
+                                <input id="cpf" name="cpf" placeholder="Apenas números" class="form-control input-md" required="" type="text" maxlength="11" pattern="[0-9]+$">
+                            </div>
+
+                            <label class="col-md-1 control-label" for="Nome">Nascimento<h11>*</h11></label>  
+                            <div class="col-md-2">
+                                <input id="dtnasc" name="dtnasc" placeholder="DD/MM/AAAA" class="form-control input-md" required="" type="text" maxlength="10" OnKeyPress="formatar('##/##/####', this)" onBlur="showhide()">
+                            </div>
+
+                            <!-- Multiple Radios (inline) -->
+
+                            <label class="col-md-1 control-label" for="radios">Sexo <h11>*</h11></label>
+                            <div class="col-md-4"> 
+                                <label required="" class="radio-inline" for="radios-0" >
+                                    <input name="sexo" id="sexo" value="feminino" type="radio" required>
+                                    Feminino
+                                </label> 
+                                <label class="radio-inline" for="radios-1">
+                                    <input name="sexo" id="sexo" value="masculino" type="radio">
+                                    Masculino
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Prepended text-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="prependedtext">Telefone <h11>*   </h11></label>
+                            <div class="col-md-3">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                                    <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" required="" type="text" maxlength="13" pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
+                                           OnKeyPress="formatar('## #####-####', this)">
+                                </div>
+                            </div>
+
+                            <label class="col-md-1 control-label" for="prependedtext">Telefone</label>
+                            <div class="col-md-2">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                                    <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" type="text" maxlength="13"  pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
+                                           OnKeyPress="formatar('## #####-####', this)">
+                                </div>
+                            </div>
+                        </div> 
+
+                        <!-- Prepended text-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="prependedtext">Email <h11>*</h11></label>
+                            <div class="col-md-5">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                    <input id="prependedtext" name="prependedtext" class="form-control" placeholder="email@email.com" required="" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" >
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <!-- Search input-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="CEP">CEP <h11>*</h11></label>
+                            <div class="col-md-2">
+                                <input id="cep" name="cep" placeholder="Apenas números" class="form-control input-md" required="" value="" type="search" maxlength="8" pattern="[0-9]+$">
+                            </div>
+                            <div class="col-md-2">
+                                <button type="button" class="btn btn-primary" onclick="pesquisacep(cep.value)">Pesquisar</button>
+                            </div>
+                        </div>
+
+                        <!-- Prepended text-->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="prependedtext">Endereço</label>
+                            <div class="col-md-3">
+                                <div class="input-group">
+                                    <span class="input-group-addon">Rua</span>
+                                    <input id="rua" name="rua" class="form-control" placeholder="" required="" readonly="readonly" type="text">
+                                </div>
+
+                            </div>
+                            <div class="col-md-2">
+                                <div class="input-group">
+                                    <span class="input-group-addon">Nº <h11>*</h11></span>
+                                    <input id="numero" name="numero" class="form-control" placeholder="" required=""  type="text">
+                                </div>
+
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="input-group">
+                                    <span class="input-group-addon">Bairro</span>
+                                    <input id="bairro" name="bairro" class="form-control" placeholder="" required="" readonly="readonly" type="text">
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="prependedtext"></label>
+                            <div class="col-md-4">
+                                <div class="input-group">
+                                    <span class="input-group-addon">Cidade</span>
+                                    <input id="cidade" name="cidade" class="form-control" placeholder="" required=""  readonly="readonly" type="text">
+                                </div>
+
+                            </div>
+
+                            <div class="col-md-2">
+                                <div class="input-group">
+                                    <span class="input-group-addon">Estado</span>
+                                    <input id="estado" name="estado" class="form-control" placeholder="" required=""  readonly="readonly" type="text">
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- Button (Double) -->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="Cadastrar"></label>
+                            <div class="col-md-8">
+                                <button id="Cadastrar" name="Cadastrar" class="btn btn-success" type="Submit">Cadastrar</button>
+                                <button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </fieldset>
+        </form>
+    </body>
+    <script src="../Assets/Js/Script.js"></script>
+</html>
